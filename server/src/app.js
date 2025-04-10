9a0ac9df135a21dee6fa2320ffce9bb8c4c9a3a5
Jeyson Yanez <jeyson2022yanez@gmail.com>
2025-04-10 13:29:43 -0600

    vdeployed

--- server/src/app.js
+++ server/src/app.js
@@ -27,4 +27,14 @@ app.use("/api", commentsRoutes);
 app.use("/api", contentRoutes);
 app.use("/api", categoryRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("../../client/dist"));
+
+  app.get("*", (req, res) => {
+    console.log(path.resolve("client", "dist", "index.html") );
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+}
+
 export default app;
---
+++ server/src/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import crypto from 'crypto';
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 4000;
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongo:27017/mern-recipes";
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || crypto.randomBytes(64).toString('hex');
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://192.168.101.250";
